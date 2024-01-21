9c440325f7362d37c47997a097522c1db23655a1
“shreyaansjain06” <shreyaansjain60@gmail.com>
2024-01-21 16:35:30 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,34 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Dashbaord from "./pages/Dashbaord";
-import Projects from "./pages/Projects";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashbaord from './pages/Dashbaord';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+
 export default function App() {
-  return <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/sign-in" element={<SignIn/>}></Route>
-    <Route path="/sign-up" element={<SignUp/>}></Route>
-    <Route path="/dashboard" element={<Dashbaord/>}></Route>
-    <Route path="/projects" element={<Projects/>}></Route>
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashbaord />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
