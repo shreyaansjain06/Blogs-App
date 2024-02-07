4db1b6327b9c1cd3f6d54dc82518e44bdd238f49
“shreyaansjain06” <shreyaansjain60@gmail.com>
2024-02-07 10:57:27 +0530

    show users to the admin dashboard

--- client/src/pages/Dashbaord.jsx
+++ client/src/pages/Dashbaord.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-import DashPosts from '../components/DashPosts.jsx';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers.jsx';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -24,6 +25,8 @@ export default function Dashboard() {
       {tab === 'profile' && <DashProfile />}
       {/* posts... */}
       {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
     </div>
   );
 }
